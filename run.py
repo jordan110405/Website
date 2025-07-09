ffcfbfef2c317496ad4f26c022f713e8e3667850
jordan110405 <ridoputra1112@gmail.com>
2025-07-10 06:36:49 +0800

    Update run.py

--- run.py
+++
@@ -1,9 +0,0 @@
-from myapp import create_app
-from myapp import db
-
-if __name__ == '__main__':
-    app = create_app()
-    '''with app.app_context():
-        db.drop_all()
-        db.create_all()'''
-    app.run()
